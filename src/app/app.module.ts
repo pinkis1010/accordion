2df196ae880ea2b361819b1844e1733cacde9331
gour9808 <gouravsharma614@gmail.com>
2020-10-16 20:46:32 +0530

    added accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EPrescriptionComponent } from './e-prescription/e-prescription.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EPrescriptionComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
+,
+    AccordionModule.forRoot()
   ],
   providers: [],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/e-prescription/e-prescription.component.ts
@@ -0,0 +1,36 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation,
+  ViewChild,
+  HostListener,
+  ElementRef,
+  OnDestroy,
+  AfterViewInit,
+  ChangeDetectorRef
+} from '@angular/core';
+
+@Component({
+  selector: 'app-e-prescription',
+  templateUrl: './e-prescription.component.html',
+  styleUrls: ['./e-prescription.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+ 
+})
+export class EPrescriptionComponent
+  implements OnInit {
+  
+
+  constructor(
+   
+  ) {
+    
+  }
+
+  ngOnInit() {
+   
+  }
+}
+
+  
